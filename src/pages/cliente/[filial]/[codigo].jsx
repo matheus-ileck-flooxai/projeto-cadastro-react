3eb287a8dfcc891368793ce14c681f449cd77c92
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-14 12:13:41 -0300

    feat: add dynamic navigation

---
+++ src/pages/cliente/[filial]/[codigo].jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import Layout from "../../../components/Layout";
+import { useRouter } from "next/router";
+
+export default function ClienteProCodigo(){
+
+
+    const router = useRouter()
+
+  
+
+    return (
+        <Layout titulo="Navegação Dinâmica">
+
+            <div>Código = {router.query.codigo}</div>
+            <div>Filial = {router.query.filial}</div>
+
+
+        </Layout>
+
+    )
+
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,10 @@ export default function Inicio() {
       <Navegador destino="/estiloso" texto="Estiloso"></Navegador>
       <Navegador destino="/exemplo"texto="Exemplo" color="#9400d3"></Navegador>
       <Navegador destino="/jsx" texto="Jsx" color="crimson"></Navegador>
+      <Navegador destino="/navegacao" texto="Navegação  #01" color="crimson"></Navegador>
+      <Navegador destino="/cliente/sp-2/123" texto="Navegação  #02" color="crimson"></Navegador>
+
+
 
     </div>
   )
