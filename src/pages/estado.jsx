10590e7e489db29fc52ea78eb6a9bf062e387d31
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-14 12:25:32 -0300

    feat: add component with state

---
+++ src/pages/estado.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Layout from "../components/Layout";
+
+export default function Estado() {
+
+    const [numero, setNumero] = useState(0)
+
+    function incrementar(){
+
+        setNumero(numero + 1)
+        
+
+    }
+    return (
+        
+        <Layout titulo="Componente com Estado">
+
+            <div>{numero}</div>
+            <button onClick={incrementar}>+1</button>
+
+        </Layout>
+
+    )
+    
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,9 @@ export default function Inicio() {
       <Navegador destino="/exemplo"texto="Exemplo" color="#9400d3"></Navegador>
       <Navegador destino="/jsx" texto="Jsx" color="crimson"></Navegador>
       <Navegador destino="/navegacao" texto="Navegação  #01" color="crimson"></Navegador>
-      <Navegador destino="/cliente/sp-2/123" texto="Navegação  #02" color="crimson"></Navegador>
+      <Navegador destino="/cliente/sp-2/123" texto="Navegação  #02" color="blue"></Navegador>
+      <Navegador destino="/estado" texto="Componente com Estado" color="pink"></Navegador>
+
 
 
 
